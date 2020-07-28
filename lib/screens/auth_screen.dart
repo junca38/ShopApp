3feb1c7dd95d576f354a8fd5bdd14b007b6fcf52
junca38 <whitefloweroil@gmail.com>
2020-07-28 20:37:07 +0800

    using AnimationContainer instead

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -213,16 +213,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heighAnimation,
-        builder: (context, childForm) => Container(
-            //height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heighAnimation.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heighAnimation.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: childForm),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        //height: _heighAnimation.value.height,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
